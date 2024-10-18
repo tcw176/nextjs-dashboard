dcd0a928004a9a17ced02d250f32013e94d9a63b
tcw176 <tyronewong@gmail.com>
2024-10-18 16:51:38 -0400

    Ch 14 without edit-form section

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -4,12 +4,19 @@ import { z } from 'zod';
 import { sql } from '@vercel/postgres';
 import { revalidatePath } from 'next/cache';
 import { redirect } from 'next/navigation';
+import { UUID } from 'crypto';
 
 const FormSchema = z.object({
     id: z.string(),
-    customerId: z.string(),
-    amount: z.coerce.number(),
-    status: z.enum(['pending', 'paid']),
+    customerId: z.string({
+      invalid_type_error: 'Please select a customer.',
+    }),
+    amount: z.coerce
+      .number()
+      .gt(0, { message: 'Please enter an amount greater than $0.' }),
+    status: z.enum(['pending', 'paid'], {
+      invalid_type_error: 'Please select an invoice status.',
+    }),
     date: z.string(),
   });
 
@@ -18,14 +25,39 @@ const CreateInvoice = FormSchema.omit({ id: true, date: true });
 // Use Zod to update the expected types
 const UpdateInvoice = FormSchema.omit({ id: true, date: true });
 
-export async function createInvoice(formData: FormData) {
-    const { customerId, amount, status } = CreateInvoice.parse({  
+export type State = {
+  errors?: {
+    customerId?: string[];
+    amount?: string[];
+    status?: string[];
+  };
+  message?: string | null;
+};
+
+export async function createInvoice(prevState: State, formData: FormData) {
+  //Validate form fields using Zod
+  const validatedFields = CreateInvoice.safeParse({
     customerId: formData.get('customerId'),
     amount: formData.get('amount'),
     status: formData.get('status'),
   });
-  // Test it out:
-  //console.log(rawFormData);
+
+  // If form validation fails, return errors early. Otherwise, continue.
+  if (!validatedFields.success) {
+    //Did not send error to console without this.
+    console.log('validated fields object ', validatedFields);
+    console.log('THIS IS THE ERROR. ', validatedFields.error);
+    return {
+      //Returns this to where it was called from
+      errors: validatedFields.error.flatten().fieldErrors,
+      message: 'Missing Fields. Failed to Create Invoice.',
+    };
+  }
+
+  //console.log(validatedFields);
+  // Prepare data for insertion into the database
+  const { customerId, amount, status } = validatedFields.data;
+  console.log('values to put into database: ', { customerId, amount, status });
   const amountInCents = amount * 100;
   const date = new Date().toISOString().split('T')[0];
   
