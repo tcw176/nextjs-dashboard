fd69b5602f3be13c0022db13d89f70b2412c8011
tcw176 <tyronewong@gmail.com>
2024-10-23 12:08:03 -0400

    ch 16 added meta data to invoice page only

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,17 @@
 import '@/app/ui/global.css';
 import { inter } from '@/app/ui/fonts';
 import { lusitana } from '@/app/ui/fonts';
- 
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+    title: {
+      template: '%s | HeyPal Dashboard',
+      default: 'HeyPal Dashboard',
+    },
+    description: 'The HeyPal Sharing Site.',
+    metadataBase: new URL('https://heypal.com'),
+};
+
 export default function RootLayout({
   children,
 }: {
